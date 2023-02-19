2284fdfb7349bbefb4656ada1492ac8a51810c8b
zaiky09 <zamilmozamil@outlook.com>
2023-02-19 10:40:58 +0300

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user'
+
+class Student < User
+
+    def initialize 
+        @knowledge = []
+    end
+    def learn str
+        @knowledge << str
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
